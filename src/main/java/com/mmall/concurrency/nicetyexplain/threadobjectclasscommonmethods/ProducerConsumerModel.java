efb26fe7001c9b8991a755340bc65680301fc071
lucas ma <robbincen@163.com>
2019-09-11 21:08:07 +0800

    producer and consumer wait version

---
+++ src/main/java/com/mmall/concurrency/nicetyexplain/threadobjectclasscommonmethods/ProducerConsumerModel.java
@@ -0,0 +1,97 @@
+package com.mmall.concurrency.nicetyexplain.threadobjectclasscommonmethods;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @Author mcg
+ * @Date 2019/9/11 20:52
+ * <p>
+ *
+ *     生产者消费者模式的 wait 版本
+ * 描述： 用 wait/notify 来实现
+ *
+ **/
+
+public class ProducerConsumerModel {
+
+    public static void main(String[] args) {
+        EventStorage eventStorage = new EventStorage();
+        Producer producer = new Producer(eventStorage);
+        Consumer consumer = new Consumer(eventStorage);
+        new Thread(producer).start();
+        new Thread(consumer).start();
+    }
+
+}
+
+class Producer implements Runnable {
+
+    private EventStorage storage;
+
+    public Producer(EventStorage storage) {
+        this.storage = storage;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+            storage.put();
+        }
+    }
+}
+
+class Consumer implements Runnable {
+
+    private EventStorage storage;
+
+    public Consumer(EventStorage storage) {
+        this.storage = storage;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+            storage.take();
+        }
+    }
+}
+
+class EventStorage {
+    private int maxSize;
+    private LinkedList<Date> storage;
+
+    public EventStorage() {
+        maxSize = 10;
+        storage = new LinkedList<>();
+    }
+
+    public synchronized void put() {
+        while (storage.size() == maxSize) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        storage.add(new Date());
+        System.out.println("仓库里有了" + storage.size() + "个产品");
+        // 唤醒
+        notify();
+    }
+
+    public synchronized void take() {
+        while (storage.size() == 0) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println("拿到了" + storage.poll() + "，现在还剩下" + storage.size());
+        notify();
+    }
+}
