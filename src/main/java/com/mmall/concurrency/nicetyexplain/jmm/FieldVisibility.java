c55b70a8838be3ef6c7a218cfccb84af282c012b
lucas ma <robbincen@163.com>
2019-10-30 22:30:56 +0800

    volatile 关键字

--- src/main/java/com/mmall/concurrency/nicetyexplain/jmm/FieldVisibility.java
+++ src/main/java/com/mmall/concurrency/nicetyexplain/jmm/FieldVisibility.java
@@ -14,7 +14,7 @@ package com.mmall.concurrency.nicetyexplain.jmm;
 
 public class FieldVisibility {
 
-     volatile int a = 1;
+      int a = 1;
      volatile int b = 2;
 
     private void change() {
